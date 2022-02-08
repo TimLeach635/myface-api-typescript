f3b2db9497704dd36d075cd8dbedd2fceeca6004
Tim Leach <timleach635@gmail.com>
2022-02-08 15:29:56 +0000

    Added links and navbar

--- public/src/src/App.js
+++ public/src/src/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PostListPage } from './components/posts/postListPage/PostListPage';
 import { UserDetailPage } from './components/users/userDetailPage/UserDetailPage';
 import { UserListPage } from './components/users/userListPage/UserListPage';
+import { Navbar } from './components/navbar/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <header>
+          <Navbar />
+        </header>
         <Routes>
           <Route path="posts" element={<PostListPage />} />
           <Route path="users" element={<UserListPage />} />
--- public/src/src/components/users/user/User.jsx
+++ public/src/src/components/users/user/User.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export function User({ user }) {
     return <div>
-        <h2>{user.name}</h2>
+        <Link to={`/users/${user.id}`}>
+            <h2>{user.name}</h2>
+        </Link>
         <img src={user.profileImageUrl} alt={user.name} />
         <p>{user.username} - {user.email}</p>
     </div>
